3d99edb95fc06ea85fa8dee416b995f1ae97bffa
duckb-y <tploxvrdct@protonmail.ch>
2022-07-26 17:47:11 -0300

    Possible moves of a piece implementation + redefining Piece.java and Chesspiece.java scope to abstract + validateSourcePosition new exception condition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -39,12 +39,15 @@ public class ChessMatch {
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
         Piece capturedPiece = makeMove(source, target);
-        return (ChessPiece)capturedPiece;
+        return (ChessPiece) capturedPiece;
     }
 
     private void validateSourcePosition(Position position){
         if (!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is no possible moves for the chosen piece");
         }
     }
 
